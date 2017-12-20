cb9d2c9f18e3a9e2c6f28ee7a9b1230f762ae0ab
loriqing <490855882@qq.com>
2017-12-20 18:52:47 +0800

    Update

--- Xmu.Crms.ViceVersa/Program.cs
+++ Xmu.Crms.ViceVersa/Program.cs
@@ -10,24 +10,14 @@ namespace Xmu.Crms.ViceVersa
     {
         public static void Main(string[] args)
         {
-            //添加自身的RESTful API
-            //将RESTful API放在这个项目（如 ClassController.cs）
-            Startup.ControllerAssembly.Add(Assembly.GetEntryAssembly());
-
-            //添加所需要的界面
-            Startup.ControllerAssembly.Add(Assembly.GetAssembly(typeof(Web.ViceVersa.Program)));
-
-            //添加所需要的Service
-            Startup.ConfigureCrmsServices += collection => collection.AddViceVersaUserService();
-
             var host = BuildWebHost(args);
             host.Run();
         }
-
-
+        
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseIISIntegration()
+                .ConfigureServices(services => services.AddViceVersaUserService().AddCrmsView("Web.ViceVersa"))
                 .UseStartup<Startup>()
                 .Build();
     }

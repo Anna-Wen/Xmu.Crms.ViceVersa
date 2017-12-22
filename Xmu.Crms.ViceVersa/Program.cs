fc742f08f30a4c0ddfc1773e1b940126f92b9b76
Jasperheng <619099139@qq.com>
2017-12-22 20:52:16 +0800

    update

--- Xmu.Crms.ViceVersa/Program.cs
+++ Xmu.Crms.ViceVersa/Program.cs
@@ -17,11 +17,7 @@ namespace Xmu.Crms.ViceVersa
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseIISIntegration()
-<<<<<<< HEAD
-                .ConfigureServices(services => services.AddViceVersaGradeDao().AddViceVersaGradeService().AddViceVersaCourseDao().AddViceVersaCourseService().AddCrmsView("Web.ViceVersa"))
-=======
-                .ConfigureServices(services => services.AddViceVersaClassService().AddViceVersaClassDao().AddCrmsView("Web.ViceVersa"))
->>>>>>> origin/JJ's
+                .ConfigureServices(services => services.AddViceVersaClassService().AddViceVersaClassDao().AddViceVersaGradeDao().AddViceVersaGradeService().AddViceVersaCourseDao().AddViceVersaCourseService().AddCrmsView("Web.ViceVersa"))
                 .UseStartup<Startup>()
                 .Build();
     }

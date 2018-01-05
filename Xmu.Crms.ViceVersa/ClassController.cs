153132849b56ad0c94ae6402e0721cef0711c48d
luoxudan <34167281+luoxudan@users.noreply.github.com>
2018-01-05 13:23:30 +0800

    update class

--- Xmu.Crms.ViceVersa/ClassController.cs
+++ Xmu.Crms.ViceVersa/ClassController.cs
@@ -15,11 +15,24 @@ namespace Xmu.Crms.ViceVersa
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class ClassController : Controller
     {
+        /// <summary>
+        /// @author Group ViceVersa
+        /// @version 2.00
+        /// </summary>
+        
         private readonly IClassService _classService;
         private readonly ICourseService _courseService;
         private readonly IUserService _userService;
         private readonly IFixGroupService _fixGroupService;
 
+        /// <summary>
+        /// 构造函数 services注入
+        /// @author Group ViceVersa
+        /// </summary>
+        /// <param name="classService">classService</param>
+        /// <param name="courseService">courseService</param>
+        /// <param name="userService">userService</param>
+        /// <param name="fixGroupService">fixGroupService</param>
         public ClassController(IClassService classService,ICourseService courseService,IUserService userService,IFixGroupService fixGroupService)
         {
             _classService = classService;
@@ -28,6 +41,14 @@ namespace Xmu.Crms.ViceVersa
             _fixGroupService = fixGroupService;
         }
 
+        /// <summary>
+        /// 按courseName/courseTeacher查询班级.
+        /// @author Group ViceVersa
+        /// </summary>
+        /// <param name="courseTeacher">教师姓名</param>
+        /// <param name="courseName">课程名称</param>
+        /// <returns>list 班级信息</returns>
+        /// <seealso cref="M:Xmu.Crms.Shared.Service.IClassService.ListClassByUserId(System.Int64)"/>
         // GET: /class?courseName={courseName}&courseTeacher={courseTeacher}
         [HttpGet]
         public IActionResult GetClassListFromQuery([FromQuery]string courseName, [FromQuery]string courseTeacher)
@@ -80,6 +101,13 @@ namespace Xmu.Crms.ViceVersa
             }
         }
 
+        /// <summary>
+        /// 按classId查询班级.
+        /// @author Group ViceVersa
+        /// </summary>
+        /// <param name="classId">班级Id</param>
+        /// <returns>ClassVO 班级信息</returns>
+        /// <seealso cref="M:Xmu.Crms.Shared.Service.IClassService.GetClassByClassId(System.Int64)"/>
         // GET: /class/{classId}
         [HttpGet("{classId}")]
         public IActionResult GetClass(long classId)
@@ -100,6 +128,14 @@ namespace Xmu.Crms.ViceVersa
 
         }
 
+        /// <summary>
+        /// 按classId修改班级.
+        /// @author Group ViceVersa
+        /// </summary>
+        /// <param name="classId">班级Id</param>
+        /// <param name="json">班级信息</param>
+        /// <seealso cref="M:Xmu.Crms.Shared.Service.IClassService.GetClassByClassId(System.Int64)"/>
+        /// <seealso cref="M:Xmu.Crms.Shared.Service.IClassService.UpdateClassByClassId(System.Int64,ClassInfo)"/>
         // PUT: /class/{classId}
         [HttpPut("{classId}")]
         public IActionResult PutClass(long classId, [FromBody]dynamic json)
@@ -156,6 +192,13 @@ namespace Xmu.Crms.ViceVersa
             }
         }
 
+        /// <summary>
+        /// 按classId删除班级.
+        /// @author Group ViceVersa
+        /// </summary>
+        /// <param name="classId">班级Id</param>
+        /// <seealso cref="M:Xmu.Crms.Shared.Service.IClassService.GetClassByClassId(System.Int64)"/>
+        /// <seealso cref="M:Xmu.Crms.Shared.Service.IClassService.DeleteClassByClassId(System.Int64)"/>
         // DELETE: /class/{classId}
         [HttpDelete("{classId}")]
         public IActionResult DeleteClass(long classId)
@@ -186,7 +229,13 @@ namespace Xmu.Crms.ViceVersa
         }
 
 
-
+        /// <summary>
+        /// 学生按classId选择课程.
+        /// @author Group ViceVersa
+        /// </summary>
+        /// <param name="classId">班级Id</param>
+        /// <param name="json">学生信息</param>
+        /// <seealso cref="M:Xmu.Crms.Shared.Service.IClassService.InsertCourseSelectionById(System.Int64,System.Int64)"/>
         // Post: /class/{classId}/student
         [HttpPost("{classId}/student")]
         public IActionResult PostStudentUnderClass(long classId, [FromBody]dynamic json)
@@ -211,7 +260,13 @@ namespace Xmu.Crms.ViceVersa
             catch (ClassNotFoundException) { return NotFound("2"); }
         }
 
-
+        /// <summary>
+        /// 学生按classId退选课程.
+        /// @author Group ViceVersa
+        /// </summary>
+        /// <param name="classId">班级Id</param>
+        /// <param name="studentId">学生Id</param>
+        /// <seealso cref="M:Xmu.Crms.Shared.Service.IClassService.DeleteCourseSelectionById(System.Int64,System.Int64)"/>
         // DELETE: /class/{classId}/student/{studentId}
         [HttpDelete("{classId}/student/{studentId}")]
         public IActionResult DeleteStudentUnderClass(long classId, long studentId)
@@ -234,6 +289,17 @@ namespace Xmu.Crms.ViceVersa
             }
         }
 
+        /// <summary>
+        /// 按studentNumber/studentName查询学生.
+        /// @author Group ViceVersa
+        /// </summary>
+        /// <param name="classId">班级Id</param>
+        /// <param name="studentNumber">学生学号</param>
+        /// <param name="studentName">学生姓名</param>
+        /// <returns>studentVO 学生信息</returns>
+        /// <seealso cref="M:Xmu.Crms.Shared.Service.IUserService.ListUserByClassId(System.Int64,String,String)"/>
+        /// <seealso cref="M:Xmu.Crms.Shared.Service.IFixGroupService.GetFixedGroupById(System.Int64,System.Int64)"/>
+        /// <seealso cref="M:Xmu.Crms.Shared.Service.IFixGroupService.ListFixGroupMemberByGroupId(System.Int64)"/>
         // GET: /Class/{classId}/student?numBeginWith={studentNumber}&nameBeginWith={studentName}
         [HttpGet("{classId}/student")]
         public IActionResult GetStudentListUnderClass(long classId,[FromQuery]string studentNumber, [FromQuery]string studentName)
@@ -269,7 +335,14 @@ namespace Xmu.Crms.ViceVersa
             
         }
 
-
+        /// <summary>
+        /// 按classId查询学生固定分组.
+        /// @author Group ViceVersa
+        /// </summary>
+        /// <param name="classId">班级Id</param>
+        /// <returns>classGroupVO 固定分组信息</returns>
+        /// <seealso cref="M:Xmu.Crms.Shared.Service.IFixGroupService.GetFixedGroupById(System.Int64,System.Int64)"/>
+        /// <seealso cref="M:Xmu.Crms.Shared.Service.IFixGroupService.ListFixGroupMemberByGroupId(System.Int64)"/>
         // GET: /class/{classId}/classgroup
         [HttpGet("{classId}/classgroup")]
         public IActionResult GetClassGroup(long classId)
@@ -300,6 +373,14 @@ namespace Xmu.Crms.ViceVersa
             }
         }
 
+        /// <summary>
+        /// 添加学生固定分组组员.
+        /// @author Group ViceVersa
+        /// </summary>
+        /// <param name="classId">班级Id</param>
+        /// <param name="json">学生信息</param>
+        /// <seealso cref="M:Xmu.Crms.Shared.Service.IFixGroupService.GetFixedGroupById(System.Int64,System.Int64)"/>
+        /// <seealso cref="M:Xmu.Crms.Shared.Service.IFixGroupService.InsertStudentIntoGroup(System.Int64,System.Int64)"/>
         // PUT: /class/{classId}/classgroup/add
         [HttpPut("{classId}/classgroup/add")]
         public IActionResult AddMemberIntoClassGroup(long classId, [FromBody]dynamic json)
@@ -330,6 +411,14 @@ namespace Xmu.Crms.ViceVersa
             }
         }
 
+        /// <summary>
+        /// 删除学生固定分组组员.
+        /// @author Group ViceVersa
+        /// </summary>
+        /// <param name="classId">班级Id</param>
+        /// <param name="json">学生信息</param>
+        /// <seealso cref="M:Xmu.Crms.Shared.Service.IFixGroupService.GetFixedGroupById(System.Int64,System.Int64)"/>
+        /// <seealso cref="M:Xmu.Crms.Shared.Service.IFixGroupService.DeleteFixGroupUserById(System.Int64,System.Int64)"/>
         // PUT: /class/{classId}/classgroup/remove
         [HttpPut("{classId}/classgroup/remove")]
         public IActionResult RemoveMemberIntoClassGroup(long classId, [FromBody]dynamic json)
